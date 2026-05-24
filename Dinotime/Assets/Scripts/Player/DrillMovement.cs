f02fafc8cf8eb17fe6d421bd56313edf41636058
Z4V <106479376+F0R5371@users.noreply.github.com>
2026-05-23 19:18:03 -0500

    Squashed commit of the following:
    commit af5f57285cb2278d444291c775791f7ffdceb8f3
    Author: Z4V <106479376+F0R5371@users.noreply.github.com>
    Date:   Sat May 23 19:16:14 2026 -0500
    
        Working on vertical and horizontal movement through blocks
    
    commit 1138323b68e87ab0f020e6479a8aef1e9a1fc4ea
    Author: Z4V <106479376+F0R5371@users.noreply.github.com>
    Date:   Sat May 23 19:02:06 2026 -0500
    
        Added Cinemachine camera
    
    commit 648f7fa9837de8c5a5bf36a58b447b215ffa3998
    Author: F0R5371 <aiden.zavaleta@proton.me>
    Date:   Sat May 23 18:24:30 2026 -0500
    
        Update Drill.unity
    
    commit 523399928677c5c9fa967e9d792feb7680806e17
    Author: F0R5371 <aiden.zavaleta@proton.me>
    Date:   Sat May 23 18:24:02 2026 -0500
    
        Added very basic movement as well as rotation of heading direction

---
+++ Dinotime/Assets/Scripts/Player/DrillMovement.cs
@@ -0,0 +1,91 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DrillMovement : MonoBehaviour
+{
+    public float speed;
+
+    private bool isMining = false;
+
+    private float headingAngle = 0;
+    private Vector2 inputDir;
+
+    private GameObject followCamera;
+    private Rigidbody2D rb;
+    private SpriteRenderer sp;
+
+    public void SwitchToDrill(Transform player)
+    {
+        print("Camera moves to focus on drill, player now moves with drill.");
+
+        player.Find("FollowCamera").gameObject.SetActive(false);
+
+        followCamera.SetActive(true);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        followCamera = transform.Find("FollowCamera").gameObject;
+
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        View();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        inputDir = new Vector2(x, y);
+
+        rb.linearVelocity = new Vector2(x * speed, 0);
+
+        if (x > 0)
+        {
+            sp.flipX = false;
+        }
+        else
+        {
+            sp.flipX = true;
+        }
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            headingAngle += 90;
+
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, headingAngle));
+        }
+    }
+
+    private void View()
+    {
+        Vector2 headingDir = new Vector2(Mathf.Cos(Mathf.Deg2Rad * headingAngle), Mathf.Sin(Mathf.Deg2Rad * headingAngle));
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, headingDir, 1f, LayerMask.NameToLayer("Mineable"));
+        Debug.DrawRay(transform.position, headingDir * 1, Color.red);
+
+        if (hit)
+        {
+            if (inputDir == headingDir)
+            {
+                isMining = true;
+            }
+        }
+    }
+}
